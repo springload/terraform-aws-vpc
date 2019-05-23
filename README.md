0bbab97315c104db7e88b16db571a5dc7ee5ddbe
Eugene Dementiev <eugene@springload.co.nz>
2019-05-24 10:38:33 +1200

    Sort out the documentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# terraform-aws-vpc for terraform >= 0.12
+
+This module configures VPC. It supports creating NAT instances (one or multiple per subnet).
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "cidr_block" {
+  description = "CIDR block of the new VPC"
+}
+variable "vpc_name" {
+  description = "VPC name"
+}
+
+
+variable "tiered" {
+  description = "Create tiered subnet configuration: private/public subnets"
+  default     = true
+}
+
+variable "tiered_nat" {
+  description = "Create one NAT instance for all private subnets"
+  default     = false
+}
+
+variable "tiered_multi_nat" {
+  description = "Create multiple NAT instances (one per private subnet)"
+  default     = false
+}
+
+locals {
+  subnets_count = "${length(data.aws_availability_zones.az.names)}"
+  nat           = var.tiered_nat || var.tiered_multi_nat
+}
